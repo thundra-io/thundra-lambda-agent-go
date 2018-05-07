e4276d3596aebb831f9951a3ab3f6e6d9356858b
doguscan <doguscan@opsgenie.com>
2018-05-07 14:18:49 +0300

    Added support for setting collector url as an environment variable

--- thundra/constants.go
+++ thundra/constants.go
@@ -1,8 +1,10 @@
 package thundra
 
-const collectorUrl = "https://collector.thundra.io/api/monitor-datas"
+const defaultCollectorUrl = "https://collector.thundra.io/api"
+const monitorDatas = "/monitor-datas"
 const DataFormatVersion = "1.1"
 
 //Thundra
 const thundraApiKey = "thundra_apiKey"
 const thundraLambdaPublishCloudwatchEnable = "thundra_lambda_publish_cloudwatch_enable"
+const thundraLambdaPublishRestBaseUrl = "thundra_lambda_publish_rest_baseUrl"
