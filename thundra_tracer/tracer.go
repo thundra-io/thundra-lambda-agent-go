76b319be8977cd0f0657811041216458efe349e4
doguscan <namal.doguscan@gmail.com>
2018-07-12 15:15:57 +0300

    Added opentracing support for manual instrumentation

---
+++ thundra_tracer/tracer.go
@@ -0,0 +1,41 @@
+package thundra_tracer
+
+import (
+	ot "github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+)
+
+// NewWithOptions creates a customized Tracer.
+func NewWithOptions(opts Options) ot.Tracer {
+	rval := &tracerImpl{opts: opts}
+	return rval
+}
+
+// New creates and returns a standard Tracer which defers completed Spans to
+// `recorder`.
+func New(recorder SpanRecorder) ot.Tracer {
+	opts := DefaultOptions()
+	opts.Recorder = recorder
+	return NewWithOptions(opts)
+}
+
+type tracerImpl struct {
+	opts Options
+}
+
+// StartSpan starts a new span with options and returns it.
+func (t *tracerImpl) StartSpan(operationName string, sso ...ot.StartSpanOption) ot.Span {
+	return newSpan(operationName, t, sso)
+}
+
+// TODO Will be implemented
+func (tracer *tracerImpl) Inject(sc ot.SpanContext, format interface{}, carrier interface{}) error {
+	panic(errors.New("Inject has not been supported yet"))
+	return errors.New("Inject has not been supported yet")
+}
+
+// TODO Will be implemented
+func (tracer *tracerImpl) Extract(format interface{}, carrier interface{}) (ot.SpanContext, error) {
+	panic(errors.New("Inject has not been supported yet"))
+	return nil, errors.New("Extract has not been supported yet")
+}
---
+++ thundra_tracer/tracer_options.go
@@ -0,0 +1,26 @@
+package thundra_tracer
+
+// Options allows creating a customized Tracer via NewWithOptions.
+type Options struct {
+	// MaxLogsPerSpan limits the number of Logs in a span (if set to a nonzero
+	// value). If a span has more logs than this value, logs are dropped as
+	// necessary (and replaced with a log describing how many were dropped).
+	//
+	// About half of the MaxLogPerSpan logs kept are the oldest logs, and about
+	// half are the newest logs.
+	//
+	// This value is ignored if DropAllLogs is true.
+	MaxLogsPerSpan int
+	// Recorder receives Spans when they have been started or finished.
+	Recorder SpanRecorder
+	// DropAllLogs turns log events on all Spans into no-ops.
+	DropAllLogs bool
+}
+
+// DefaultOptions returns an Options object with all options disabled.
+// A Recorder needs to be set manually before using the returned object with a Tracer.
+func DefaultOptions() Options {
+	return Options{
+		MaxLogsPerSpan: 100,
+	}
+}
