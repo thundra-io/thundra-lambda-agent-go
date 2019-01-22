72ccdedf0a6eccaaa6b2f673d85ac7d826e59141
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-01-22 14:55:04 +0300

    Add span start options test

--- ttracer/tracer_test.go
+++ ttracer/tracer_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/thundra-io/thundra-lambda-agent-go/ext"
+
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/thundra-io/thundra-lambda-agent-go/plugin"
@@ -12,6 +14,8 @@ import (
 const (
 	duration      = 500
 	operationName = "creating-bubble"
+	className     = "Test Class"
+	domainName    = "Test Domain"
 )
 
 func TestStartSpan(t *testing.T) {
@@ -31,4 +35,31 @@ func TestStartSpan(t *testing.T) {
 	assert.True(t, span.OperationName() == operationName)
 	assert.True(t, span.raw.ClassName == plugin.DefaultClassName)
 	assert.True(t, span.raw.DomainName == plugin.DefaultDomainName)
+}
+
+func TestStartSpanWithOptions(t *testing.T) {
+	r := NewInMemoryRecorder()
+	tracer := New(r)
+
+	f := func() opentracing.Span {
+		span := tracer.StartSpan(
+			operationName,
+			ext.ClassName(className),
+			ext.DomainName(domainName),
+			opentracing.Tag{Key: "stage", Value: "testing"},
+		)
+		defer span.Finish()
+
+		time.Sleep(time.Millisecond * duration)
+		return span
+	}
+
+	span, ok := f().(*spanImpl)
+	assert.True(t, ok)
+	assert.True(t, span.raw.Duration() >= int64(duration))
+	assert.True(t, span.OperationName() == operationName)
+	assert.True(t, span.raw.ClassName == className)
+	assert.True(t, span.raw.DomainName == domainName)
+	assert.True(t, len(span.raw.GetTags()) == 1)
+	assert.True(t, span.raw.GetTags()["stage"].(string) == "testing")
 }
