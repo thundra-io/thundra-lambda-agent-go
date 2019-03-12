b5f401e37f41c8d6784254251a0174f9896551d6
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-03-12 18:31:23 +0300

    Implement Get, Post and Do methods

--- wrappers/net/http/client.go
+++ wrappers/net/http/client.go
@@ -1,9 +1,13 @@
 package thundrahttp
 
 import (
-	"github.com/thundra-io/thundra-lambda-agent-go/ext"
-	"github.com/opentracing/opentracing-go"
+	"io"
 	"net/http"
+	"net/url"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/thundra-io/thundra-lambda-agent-go/constants"
+	"github.com/thundra-io/thundra-lambda-agent-go/ext"
 )
 
 // ClientWrapper wraps the default http.Client
@@ -16,15 +20,72 @@ func Wrap(c http.Client) ClientWrapper {
 	return ClientWrapper{c}
 }
 
-// Do wraps the http.Client.Do, starts a new span for the http call
+// Do wraps the http.Client.Do and starts a new span for the http call
 func (c *ClientWrapper) Do(req *http.Request) (resp *http.Response, err error) {
 	// TODO: Add recover method
 	span := opentracing.StartSpan(
 		req.URL.Path,
 		ext.ClassName("HTTP"),
 		ext.DomainName("API"),
+		ext.OperationType(req.Method),
 	)
 	defer span.Finish()
+	addHTTPTags(span, req.URL.String(), req.Method)
 	resp, err = c.Client.Do(req)
 	return
+}
+
+// Get wraps the http.Client.Get and starts a new span for the http call
+func (c *ClientWrapper) Get(URL string) (resp *http.Response, err error) {
+	// TODO: Add recover method
+	// Create and finish(defered) span
+	span := opentracing.StartSpan(
+		getOperationName(URL),
+		ext.ClassName("HTTP"),
+		ext.DomainName("API"),
+		ext.OperationType("GET"),
+	)
+	defer span.Finish()
+	addHTTPTags(span, URL, "GET")
+	resp, err = c.Client.Get(URL)
+	return
+}
+
+// Post wraps the http.Client.Post and starts a new span for the http call
+func (c *ClientWrapper) Post(URL, contentType string, body io.Reader) (resp *http.Response, err error) {
+	// TODO: Add recover method
+	span := opentracing.StartSpan(
+		getOperationName(URL),
+		ext.ClassName("HTTP"),
+		ext.DomainName("API"),
+		ext.OperationType("POST"),
+	)
+	defer span.Finish()
+	addHTTPTags(span, URL, "POST")
+	resp, err = c.Client.Post(URL, contentType, body)
+	return
+}
+
+func addHTTPTags(span opentracing.Span, URL, method string) {
+	// Parse URL
+	parsedURL, err := url.Parse(URL)
+
+	// Set span tags
+	span.SetTag(constants.HttpMethodTag, method)
+	if err == nil {
+		span.SetTag(constants.HttpURLTag, parsedURL.Host+parsedURL.Path)
+		span.SetTag(constants.HttpPathTag, parsedURL.Path)
+		span.SetTag(constants.HttpHostTag, parsedURL.Host)
+		span.SetTag(constants.HttpQueryParamsTag, parsedURL.Query().Encode())
+	}
+
+}
+
+func getOperationName(URL string) string {
+	// Parse URL
+	parsedURL, err := url.Parse(URL)
+	if err != nil {
+		return ""
+	}
+	return parsedURL.Host + parsedURL.Path
 }
