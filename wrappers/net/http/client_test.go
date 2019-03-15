4d1afc3b09441eb8344ce0d714dfe77a52c8fefd
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-03-15 11:04:44 +0300

    Update client_test.go

--- wrappers/net/http/client_test.go
+++ wrappers/net/http/client_test.go
@@ -2,10 +2,14 @@ package thundrahttp
 
 import (
 	"bytes"
-	"io/ioutil"
+	"context"
 	"net/http"
+	"net/url"
 	"testing"
 
+	"github.com/thundra-io/thundra-lambda-agent-go/tracer"
+
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/thundra-io/thundra-lambda-agent-go/constants"
 
 	"github.com/stretchr/testify/assert"
@@ -25,11 +29,7 @@ func NewTestClient(f RoundTripFunc) http.Client {
 }
 
 var client = Wrap(NewTestClient(func(req *http.Request) (*http.Response, error) {
-	return &http.Response{
-		StatusCode: 200,
-		Body:       ioutil.NopCloser(bytes.NewBufferString(`OK`)),
-		Header:     make(http.Header),
-	}, nil
+	return nil, http.ErrServerClosed
 }))
 
 func TestHTTPGet(t *testing.T) {
@@ -40,13 +40,216 @@ func TestHTTPGet(t *testing.T) {
 	// Get the span created for http call
 	span := tp.Recorder.GetSpans()[0]
 	// Test HTTP related fields of span
-	assert.Nil(t, err)
-	assert.NotNil(t, resp)
-	assert.Equal(t, span.ClassName, constants.HTTPClassName)
-	assert.Equal(t, span.DomainName, constants.HTTPDomainName)
-	assert.Equal(t, span.Tags[constants.HTTPHostTag].(string), "httpbin.org")
-	assert.Equal(t, span.Tags[constants.HTTPMethodTag].(string), http.MethodGet)
-	assert.Equal(t, span.Tags[constants.HTTPPathTag].(string), "/get")
-	assert.Equal(t, span.Tags[constants.HTTPQueryParamsTag].(string), "foo=bar")
-	assert.Equal(t, span.Tags[constants.HTTPURLTag].(string), "httpbin.org/get")
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+	assert.Equal(t, constants.HTTPClassName, span.ClassName)
+	assert.Equal(t, constants.HTTPDomainName, span.DomainName)
+	assert.Equal(t, "httpbin.org", span.Tags[constants.HTTPHostTag].(string))
+	assert.Equal(t, http.MethodGet, span.Tags[constants.HTTPMethodTag].(string))
+	assert.Equal(t, "/get", span.Tags[constants.HTTPPathTag].(string))
+	assert.Equal(t, "foo=bar", span.Tags[constants.HTTPQueryParamsTag].(string))
+	assert.Equal(t, "httpbin.org/get", span.Tags[constants.HTTPURLTag].(string))
+	assert.True(t, span.Tags[constants.AwsError].(bool))
+	assert.Equal(t, "Error", span.Tags[constants.AwsErrorKind].(string))
+	assert.Equal(t, "Get https://httpbin.org/get?foo=bar: http: Server closed", 
+		span.Tags[constants.AwsErrorMessage].(string))
+	// Clear tracer
+	tp.Reset()
+}
+
+func TestHTTPGetWithContext(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Create the parent span
+	ctx := context.Background()
+	parentSpan, ctx := opentracing.StartSpanFromContext(ctx, "parentSpan")
+	parentSpanRaw, _ := tracer.GetRaw(parentSpan)
+	// Actual call
+	resp, err := client.GetWithContext(ctx, "https://httpbin.org/get?foo=bar")
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[1]
+	// Check parent span is set 
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
+	assert.Equal(t, parentSpanRaw.Context.SpanID, span.ParentSpanID)
+}
+
+func TestHTTPPost(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Actual call
+	jsonStr := `{"title":"Foobar"}`
+	resp, err := client.Post("https://httpbin.org/post?foo=bar", "application/json", bytes.NewBufferString(jsonStr))
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[0]
+	// Test HTTP related fields of span
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, constants.HTTPClassName, span.ClassName)
+	assert.Equal(t, constants.HTTPDomainName, span.DomainName)
+	assert.Equal(t, "httpbin.org", span.Tags[constants.HTTPHostTag].(string))
+	assert.Equal(t, http.MethodPost, span.Tags[constants.HTTPMethodTag].(string))
+	assert.Equal(t, "/post", span.Tags[constants.HTTPPathTag].(string))
+	assert.Equal(t, "foo=bar", span.Tags[constants.HTTPQueryParamsTag].(string))
+	assert.Equal(t, "httpbin.org/post", span.Tags[constants.HTTPURLTag].(string))
+	assert.True(t, span.Tags[constants.AwsError].(bool))
+	assert.Equal(t, "Error", span.Tags[constants.AwsErrorKind].(string))
+	assert.Equal(t, "Post https://httpbin.org/post?foo=bar: http: Server closed", 
+		span.Tags[constants.AwsErrorMessage].(string))
+	// Clear tracer
+	tp.Reset()
+}
+
+func TestHTTPPostWithContext(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Create the parent span
+	ctx := context.Background()
+	parentSpan, ctx := opentracing.StartSpanFromContext(ctx, "parentSpan")
+	parentSpanRaw, _ := tracer.GetRaw(parentSpan)
+	// Actual call
+	jsonStr := `{"title":"Foobar"}`
+	resp, err := client.PostWithContext(ctx, "https://httpbin.org/post?foo=bar", "application/json", bytes.NewBufferString(jsonStr))
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[1]
+	// Check parent span is set
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, parentSpanRaw.Context.SpanID, span.ParentSpanID)
+}
+
+func TestHTTPPostForm(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Actual call
+	v := url.Values{}
+	v.Set("name", "Ava")
+	v.Add("friend", "Jess")
+	v.Add("friend", "Sarah")
+	resp, err := client.PostForm("https://httpbin.org/post?foo=bar", v)
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[0]
+	// Test HTTP related fields of span
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, constants.HTTPClassName, span.ClassName)
+	assert.Equal(t, constants.HTTPDomainName, span.DomainName)
+	assert.Equal(t, "httpbin.org", span.Tags[constants.HTTPHostTag].(string))
+	assert.Equal(t, http.MethodPost, span.Tags[constants.HTTPMethodTag].(string))
+	assert.Equal(t, "/post", span.Tags[constants.HTTPPathTag].(string))
+	assert.Equal(t, "foo=bar", span.Tags[constants.HTTPQueryParamsTag].(string))
+	assert.Equal(t, "httpbin.org/post", span.Tags[constants.HTTPURLTag].(string))
+	assert.True(t, span.Tags[constants.AwsError].(bool))
+	assert.Equal(t, "Error", span.Tags[constants.AwsErrorKind].(string))
+	assert.Equal(t, "Post https://httpbin.org/post?foo=bar: http: Server closed", 
+		span.Tags[constants.AwsErrorMessage].(string))
+	// Clear tracer
+	tp.Reset()
+}
+
+func TestHTTPPostFormWithContext(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Create the parent span
+	ctx := context.Background()
+	parentSpan, ctx := opentracing.StartSpanFromContext(ctx, "parentSpan")
+	parentSpanRaw, _ := tracer.GetRaw(parentSpan)
+	// Actual call
+	v := url.Values{}
+	v.Set("name", "Ava")
+	v.Add("friend", "Jess")
+	v.Add("friend", "Sarah")
+	resp, err := client.PostFormWithContext(ctx, "https://httpbin.org/post?foo=bar", v)
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[1]
+	// Check parent span is set
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, parentSpanRaw.Context.SpanID, span.ParentSpanID)
+}
+
+func TestHTTPDo(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Actual call
+	req, _ := http.NewRequest(http.MethodGet, "https://httpbin.org/get?foo=bar", nil)
+	resp, err := client.Do(req)
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[0]
+	// Test HTTP related fields of span
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, constants.HTTPClassName, span.ClassName)
+	assert.Equal(t, constants.HTTPDomainName, span.DomainName)
+	assert.Equal(t, "httpbin.org", span.Tags[constants.HTTPHostTag].(string))
+	assert.Equal(t, http.MethodGet, span.Tags[constants.HTTPMethodTag].(string))
+	assert.Equal(t, "/get", span.Tags[constants.HTTPPathTag].(string))
+	assert.Equal(t, "foo=bar", span.Tags[constants.HTTPQueryParamsTag].(string))
+	assert.Equal(t, "httpbin.org/get", span.Tags[constants.HTTPURLTag].(string))
+	assert.True(t, span.Tags[constants.AwsError].(bool))
+	assert.Equal(t, "Error", span.Tags[constants.AwsErrorKind].(string))
+	assert.Equal(t, "Get https://httpbin.org/get?foo=bar: http: Server closed", 
+		span.Tags[constants.AwsErrorMessage].(string))
+	// Clear tracer
+	tp.Reset()
+}
+
+func TestHTTPDoWithContext(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Create the parent span
+	ctx := context.Background()
+	parentSpan, ctx := opentracing.StartSpanFromContext(ctx, "parentSpan")
+	parentSpanRaw, _ := tracer.GetRaw(parentSpan)
+	// Actual call
+	req, _ := http.NewRequest(http.MethodGet, "https://httpbin.org/get?foo=bar", nil)
+	resp, err := client.DoWithContext(ctx, req)
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[1]
+	// Check parent span is set
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, parentSpanRaw.Context.SpanID, span.ParentSpanID)
+}
+
+func TestHTTPHead(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Actual call
+	resp, err := client.Head("https://httpbin.org/head?foo=bar")
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[0]
+	// Test HTTP related fields of span
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, constants.HTTPClassName, span.ClassName)
+	assert.Equal(t, constants.HTTPDomainName, span.DomainName)
+	assert.Equal(t, "httpbin.org", span.Tags[constants.HTTPHostTag].(string))
+	assert.Equal(t, http.MethodHead, span.Tags[constants.HTTPMethodTag].(string))
+	assert.Equal(t, "/head", span.Tags[constants.HTTPPathTag].(string))
+	assert.Equal(t, "foo=bar", span.Tags[constants.HTTPQueryParamsTag].(string))
+	assert.Equal(t, "httpbin.org/head", span.Tags[constants.HTTPURLTag].(string))
+	assert.True(t, span.Tags[constants.AwsError].(bool))
+	assert.Equal(t, "Error", span.Tags[constants.AwsErrorKind].(string))
+	assert.Equal(t, "Head https://httpbin.org/head?foo=bar: http: Server closed", 
+		span.Tags[constants.AwsErrorMessage].(string))
+	// Clear tracer
+	tp.Reset()
+}
+
+func TestHTTPHeadWithContext(t *testing.T) {
+	// Initilize trace plugin to set GlobalTracer of opentracing
+	tp := trace.New()
+	// Create the parent span
+	ctx := context.Background()
+	parentSpan, ctx := opentracing.StartSpanFromContext(ctx, "parentSpan")
+	parentSpanRaw, _ := tracer.GetRaw(parentSpan)
+	// Actual call
+	resp, err := client.HeadWithContext(ctx, "https://httpbin.org/head?foo=bar")
+	// Get the span created for http call
+	span := tp.Recorder.GetSpans()[1]
+	// Test HTTP related fields of span
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, parentSpanRaw.Context.SpanID, span.ParentSpanID)
 }
