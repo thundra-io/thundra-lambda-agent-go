2f80d47aaa1723cf6ae98f44aa81ca6f1692838c
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-04-10 14:56:43 +0300

    Delete pq dependency

--- wrappers/rdb/postgresql.go
+++ wrappers/rdb/postgresql.go
@@ -2,10 +2,11 @@ package thundrardb
 
 import (
 	"fmt"
+	"net"
+	nurl "net/url"
+	"sort"
 	"strings"
 	"unicode"
-
-	"github.com/lib/pq"
 
 	"github.com/thundra-io/thundra-lambda-agent-go/application"
 	"github.com/thundra-io/thundra-lambda-agent-go/config"
@@ -73,7 +74,7 @@ func parseOptsFromDsn(dsn string) (values, error) {
 	var err error
 	o := make(values)
 	if strings.HasPrefix(dsn, "postgres://") || strings.HasPrefix(dsn, "postgresql://") {
-		dsn, err = pq.ParseURL(dsn)
+		dsn, err = parseURL(dsn)
 		if err != nil {
 			return nil, err
 		}
@@ -195,4 +196,50 @@ func parseOpts(name string, o values) error {
 	}
 
 	return nil
+}
+
+func parseURL(url string) (string, error) {
+	u, err := nurl.Parse(url)
+	if err != nil {
+		return "", err
+	}
+
+	if u.Scheme != "postgres" && u.Scheme != "postgresql" {
+		return "", fmt.Errorf("invalid connection protocol: %s", u.Scheme)
+	}
+
+	var kvs []string
+	escaper := strings.NewReplacer(` `, `\ `, `'`, `\'`, `\`, `\\`)
+	accrue := func(k, v string) {
+		if v != "" {
+			kvs = append(kvs, k+"="+escaper.Replace(v))
+		}
+	}
+
+	if u.User != nil {
+		v := u.User.Username()
+		accrue("user", v)
+
+		v, _ = u.User.Password()
+		accrue("password", v)
+	}
+
+	if host, port, err := net.SplitHostPort(u.Host); err != nil {
+		accrue("host", u.Host)
+	} else {
+		accrue("host", host)
+		accrue("port", port)
+	}
+
+	if u.Path != "" {
+		accrue("dbname", u.Path[1:])
+	}
+
+	q := u.Query()
+	for k := range q {
+		accrue(k, q.Get(k))
+	}
+
+	sort.Strings(kvs) // Makes testing easier (not a performance concern)
+	return strings.Join(kvs, " "), nil
 }
