3bbb5bc9d25d087c567582457916215b9deedc39
Hamit Burak Emre <hamitburakemre@gmail.com>
2019-10-21 14:08:05 +0300

    Add base64 and gzip decoding

--- tracer/tracer_support.go
+++ tracer/tracer_support.go
@@ -1,7 +1,11 @@
 package tracer
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -32,7 +36,21 @@ func ParseSpanListeners() {
 			config := make(map[string]interface{})
 			splits := strings.SplitN(env, "=", 2)
 
-			if err := json.Unmarshal([]byte(splits[1]), &config); err != nil {
+			if len(splits) < 2 {
+				continue
+			}
+
+			var err error
+			configStr := splits[1]
+
+			if !strings.HasPrefix(configStr, "{") {
+				configStr, err = decodeConfigStr(configStr)
+				if err != nil {
+					// TODO: Handle config decode error
+				}
+			}
+
+			if err := json.Unmarshal([]byte(configStr), &config); err != nil {
 				// TODO: Handle json unmarshal error
 				continue
 			}
@@ -61,6 +79,25 @@ func ParseSpanListeners() {
 			}
 		}
 	}
+}
+
+func decodeConfigStr(configStr string) (string, error) {
+	z, err := base64.StdEncoding.DecodeString(configStr)
+	if err != nil {
+		return "", err
+	}
+
+	r, err := gzip.NewReader(bytes.NewReader(z))
+	if err != nil {
+		return "", err
+	}
+
+	result, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result), nil
 }
 
 func init() {
