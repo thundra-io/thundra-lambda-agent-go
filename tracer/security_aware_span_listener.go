4c2cce73ee5702883813780fe44f2768caaee296
Fatih Aydilek <fatihaydilek@gmail.com>
2019-11-13 16:24:08 +0300

    add security aware span listener

--- tracer/security_aware_span_listener.go
+++ tracer/security_aware_span_listener.go
@@ -2,9 +2,10 @@ package tracer
 
 import (
 	"errors"
+	"reflect"
+
 	"github.com/thundra-io/thundra-lambda-agent-go/constants"
 	"github.com/thundra-io/thundra-lambda-agent-go/utils"
-	"reflect"
 )
 
 var defaultSecurityMessage = "Operation was blocked due to security configuration"
@@ -22,6 +23,7 @@ func (s *SecurityAwareSpanListener) OnSpanStarted(span *spanImpl) {
 
 	for _, op := range s.blacklist {
 		if op.matches(span) {
+			s.handleSecurityIssue(span)
 			return
 		}
 	}
@@ -49,6 +51,7 @@ func (s *SecurityAwareSpanListener) handleSecurityIssue(span *spanImpl) {
 	if s.block {
 		err := errors.New(defaultSecurityMessage)
 		span.SetTag(constants.SecurityTags["BLOCKED"], true)
+		span.SetTag(constants.SecurityTags["VIOLATED"], true)
 		utils.SetSpanError(span, err)
 		panic(err)
 	} else {
@@ -61,9 +64,8 @@ func (s *SecurityAwareSpanListener) isExternalOperation(span *spanImpl) bool {
 }
 
 type Operation struct {
-	className      string
-	operationTypes []string
-	tags           map[string]interface{}
+	className string
+	tags      map[string]interface{}
 }
 
 func (o *Operation) matches(span *spanImpl) bool {
@@ -73,30 +75,37 @@ func (o *Operation) matches(span *spanImpl) bool {
 		matched = o.className == span.raw.ClassName
 	}
 
-	if matched && len(o.operationTypes) > 0 {
-		matched = utils.Contains(o.operationTypes, o.getOperationType(span))
-	}
-
 	if matched && len(o.tags) > 0 {
 		for key, value := range o.tags {
-			rt := reflect.TypeOf(value)
+			if span.raw.GetTag(key) != nil {
 
-			if (rt.Kind() == reflect.Slice || rt.Kind() == reflect.Array) &&
-				utils.Contains(value.([]string), span.raw.GetTag(key).(string)) {
-				matched = false
-				break
-			} else if (rt.Kind() != reflect.Slice || rt.Kind() != reflect.Array) &&
-				span.raw.GetTag(key) != value {
-				matched = false
-				break
+				rt := reflect.TypeOf(value)
+				if rt.Kind() == reflect.Slice || rt.Kind() == reflect.Array {
+					switch value.(type) {
+					case []string:
+						matched = utils.StringContains(value.([]string), span.raw.GetTag(key).(string))
+						break
+					case []int64:
+						matched = utils.Int64Contains(value.([]int64), span.raw.GetTag(key).(int64))
+						break
+					case []float64:
+						matched = utils.Float64Contains(value.([]float64), span.raw.GetTag(key).(float64))
+						break
+					case []interface{}:
+						matched = utils.Contains(value.([]interface{}), span.raw.GetTag(key))
+						break
+					default:
+						panic("unexpected value type")
+
+					}
+				} else if (rt.Kind() != reflect.Slice || rt.Kind() != reflect.Array) &&
+					span.raw.GetTag(key) != value {
+					matched = false
+					break
+				}
 			}
 		}
 	}
 
 	return matched
-
-}
-
-func (o *Operation) getOperationType(span *spanImpl) string {
-	return span.raw.GetTag(constants.SpanTags["OPERATION_TYPE"]).(string)
 }
--- tracer/security_aware_span_listener_test.go
+++ tracer/security_aware_span_listener_test.go
@@ -1,1 +1,146 @@
 package tracer
+
+import (
+	"github.com/thundra-io/thundra-lambda-agent-go/constants"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thundra-io/thundra-lambda-agent-go/ext"
+)
+
+func TestConfig(t *testing.T) {
+	sasl := SecurityAwareSpanListener{
+		block: true,
+		whitelist: []Operation{
+			{
+				className: "HTTP",
+				tags: map[string]interface{}{
+					"http.host":      []string{"www.google.com", "www.google.com"},
+					"operation.type": []string{"GET"},
+				},
+			},
+			{
+				className: "AWS-DynamoDB",
+				tags: map[string]interface{}{
+					"aws.dynamodb.table.name": []string{"Users"},
+					"operation.type":          []string{"READ"},
+				},
+			},
+		},
+		blacklist: []Operation{
+			{
+				className: "HTTP",
+				tags: map[string]interface{}{
+					"http.host":      []string{"www.foo.com", "www.foo.com"},
+					"operation.type": []string{"POST"},
+				},
+			},
+			{
+				className: "AWS-SNS",
+				tags: map[string]interface{}{
+					"aws.sns.topic.name": []string{"foo-topic"},
+					"operation.type":     []string{"WRITE"},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, true, sasl.block)
+	assert.Equal(t, 2, len(sasl.whitelist))
+	assert.Equal(t, 2, len(sasl.blacklist))
+
+	assert.Equal(t, "HTTP", sasl.whitelist[0].className)
+	assert.Equal(t, []string{"www.google.com", "www.google.com"}, sasl.whitelist[0].tags["http.host"])
+	assert.Equal(t, []string{"GET"}, sasl.whitelist[0].tags["operation.type"])
+	assert.Equal(t, "www.google.com", sasl.whitelist[0].tags["http.host"].([]string)[0])
+
+	assert.Equal(t, "AWS-SNS", sasl.blacklist[1].className)
+	assert.Equal(t, []string{"foo-topic"}, sasl.blacklist[1].tags["aws.sns.topic.name"])
+	assert.Equal(t, []string{"WRITE"}, sasl.blacklist[1].tags["operation.type"])
+	assert.Equal(t, "foo-topic", sasl.blacklist[1].tags["aws.sns.topic.name"].([]string)[0])
+}
+
+func TestWhiteListSpan(t *testing.T) {
+	sasl := SecurityAwareSpanListener{
+		block: true,
+		whitelist: []Operation{
+			{
+				className: "HTTP",
+				tags: map[string]interface{}{
+					"http.host":      []string{"www.google.com", "www.facebook.com"},
+					"operation.type": []string{"GET"},
+				},
+			},
+			{
+				className: "AWS-DynamoDB",
+				tags: map[string]interface{}{
+					"aws.dynamodb.table.name": []string{"Users"},
+					"operation.type":          []string{"READ"},
+				},
+			},
+		},
+	}
+
+	tracer, _ := newTracerAndRecorder()
+
+	s1 := tracer.StartSpan("foo", ext.ClassName("HTTP"), ext.OperationType("GET"))
+	s1.SetTag("http.host", "www.google.com")
+	s1.SetTag("topology.vertex", true)
+
+	s2 := tracer.StartSpan("foo", ext.ClassName("HTTP"), ext.OperationType("GET"))
+	s2.SetTag("http.host", "www.google.com")
+	s2.SetTag("topology.vertex", true)
+
+	sasl.OnSpanStarted(s1.(*spanImpl))
+	sasl.OnSpanStarted(s2.(*spanImpl))
+
+	assert.Equal(t, nil, s1.(*spanImpl).raw.GetTag(constants.SecurityTags["BLOCKED"]))
+	assert.Equal(t, nil, s1.(*spanImpl).raw.GetTag(constants.SecurityTags["VIOLATED"]))
+	assert.Equal(t, nil, s2.(*spanImpl).raw.GetTag(constants.SecurityTags["BLOCKED"]))
+	assert.Equal(t, nil, s2.(*spanImpl).raw.GetTag(constants.SecurityTags["VIOLATED"]))
+
+	s3 := tracer.StartSpan("foo", ext.ClassName("HTTP"), ext.OperationType("POST"))
+	s3.SetTag("http.host", "www.example.com")
+	s3.SetTag("topology.vertex", true)
+
+	var errorPanicked3 error
+	func() {
+		defer func() {
+			errorPanicked3 = recover().(error)
+			assert.Equal(t, "Operation was blocked due to security configuration", errorPanicked3.Error())
+			assert.Equal(t, true, s3.(*spanImpl).raw.GetTag(constants.SecurityTags["BLOCKED"]))
+			assert.Equal(t, true, s3.(*spanImpl).raw.GetTag(constants.SecurityTags["VIOLATED"]))
+		}()
+		sasl.OnSpanStarted(s3.(*spanImpl))
+	}()
+
+	s4 := tracer.StartSpan("foo", ext.ClassName("AWS-DynamoDB"), ext.OperationType("WRITE"))
+	s4.SetTag("topology.vertex", true)
+	s4.SetTag(constants.SpanTags["OPERATION_TYPE"], "WRITE")
+
+	var errorPanicked4 error
+	func() {
+		defer func() {
+			errorPanicked4 = recover().(error)
+			assert.Equal(t, "Operation was blocked due to security configuration", errorPanicked4.Error())
+			assert.Equal(t, true, s4.(*spanImpl).raw.GetTag(constants.SecurityTags["BLOCKED"]))
+			assert.Equal(t, true, s4.(*spanImpl).raw.GetTag(constants.SecurityTags["VIOLATED"]))
+		}()
+		sasl.OnSpanStarted(s4.(*spanImpl))
+	}()
+
+	s5 := tracer.StartSpan("foo", ext.ClassName("HTTP"), ext.OperationType("POST"))
+	s5.SetTag("http.host", "www.google.com")
+	s5.SetTag("topology.vertex", true)
+
+	var errorPanicked5 error
+	func() {
+		defer func() {
+			errorPanicked5 = recover().(error)
+			assert.Equal(t, "Operation was blocked due to security configuration", errorPanicked5.Error())
+			assert.Equal(t, true, s5.(*spanImpl).raw.GetTag(constants.SecurityTags["BLOCKED"]))
+			assert.Equal(t, true, s5.(*spanImpl).raw.GetTag(constants.SecurityTags["VIOLATED"]))
+		}()
+		sasl.OnSpanStarted(s5.(*spanImpl))
+	}()
+}
